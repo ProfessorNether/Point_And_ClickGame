78fa85c752d98390fa902fbbcebf8fcb956a3a5a
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2024-02-07 01:45:05 +0100

    pixel perfection

--- js/Main.js
+++ js/Main.js
@@ -1,2 +1,2 @@
-alert('<!DOCTYPE html> <html> <head> <meta name="viewport" content="width=device-width, initial-scale=1"> <style>.square { height: 50px; width: 50px; background-color: #555;}</style>/head><body><h2>Square CSS</h2> <div class="square"></div></body></html> ');
+alert('Welcome User To The Point And Click Game');
 document.getElementById("MainTitle").innerText = "Point & Click Game";
