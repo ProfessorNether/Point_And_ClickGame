33e6c86df39b4fb939858004e4c12a7605767095
LucasLimoenMan <140121885+ProfessorNether@users.noreply.github.com>
2024-02-09 14:17:09 +0100

    Point and Click game art

--- js/Main.js
+++ js/Main.js
@@ -1,2 +1,39 @@
-alert('Welcome User To The Point And Click Game');
+alert('Deze pagina heeft toegang tot uw huidige locatie & zoekgeschiedenis');
 document.getElementById("MainTitle").innerText = "Point & Click Game";
+//Game window ref
+const GameWindow = document.getElementById("container");
+
+//main moveable char ref
+const MainCharacter = document.getElementById("MainCharacter");
+const OffssetCharacter = 40;
+
+//what to do if user clicks
+GameWindow.onclick = function (e) {
+    var rect = GameWindow.getBoundingClientRect();
+    var X = e.clientX - rect.left
+    var Y = e.clientY - rect.top
+
+    console.log(e.target.id);
+    MainCharacter.style.left = X - OffssetCharacter + "px";
+    MainCharacter.style.top = Y - OffssetCharacter + "px";
+
+    switch (e.target.id) {
+        case "Door1":
+            console.log("Open Door Bitch;");
+            Door1.style.opacity = 0.2;
+            Sign.style.opacity = 1;
+            break;
+        case "Sign":
+            console.log("Read Sign Bitch;");
+            Sign.style.opacity = 0.2;
+            Door1.style.opacity = 1;
+            break;
+
+        default:
+            MainCharacter.style.background.opacity = 0;
+            Door1.style.opacity = 1;
+            Sign.style.opacity = 1;
+            //do something
+            break;
+    }
+}
